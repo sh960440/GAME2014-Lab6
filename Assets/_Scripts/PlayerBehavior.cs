f401ae5c1bd9d18cf38e164b321d5016ebda227f
sh960440 <54958110+sh960440@users.noreply.github.com>
2020-11-04 00:01:45 +0800

    Added tilemap, player and joystick

---
+++ Assets/_Scripts/PlayerBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public Joystick jojstick;
+    public float JoystickSensitivity;
+    public float horizontalForce;
+    public float jumpForce;
+    public float maximumVelocituX;
+    public Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // _Move();
+        this._Move(); 
+    }
+
+    private void _Move()
+    {
+        if (jojstick.Horizontal > JoystickSensitivity)
+        {
+            rigidbody.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+        }
+
+        if (jojstick.Horizontal < -JoystickSensitivity)
+        {
+            rigidbody.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+        }
+    }
+}
